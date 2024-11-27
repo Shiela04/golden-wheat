6e5319092e1a67caec820f8c9fc44e4d0f11b87a
Moksham <mokshamsharma09@gmail.com>
2024-11-27 14:15:11 -0700

    tweaks in footer social icons

--- app/components/NavListItem.js
+++ app/components/NavListItem.js
@@ -3,13 +3,13 @@ import { HEADER_DETAILS } from "@/config/static-data";
 
 export default function NavListItem() {
   const listItems = HEADER_DETAILS.navMenu.map((item) => (
-    <ui key={uuidv4()}>
+    <ul key={uuidv4()}>
       <a href={item.navUrl}>{item.navText}</a>
-    </ui>
+    </ul>
   ));
   return (
     <div className="hidden sm:ml-6 sm:block">
-      <div className="flex space-x-4 items-center list-none">{listItems}</div>
+      <div className="flex items-center space-x-4 list-none">{listItems}</div>
     </div>
   );
 }
