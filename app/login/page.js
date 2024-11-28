870b64d336563715138acb114b9330d6ee004ace
Tasuku-CV6 <Tasukuseikatsu45@gmail.com>
2024-11-28 14:27:38 -0700

    Added login feature for backend

--- app/login/page.js
+++ app/login/page.js
@@ -11,10 +11,6 @@ export default function RegistrationForm() {
   const [password, setPassword] = useState("");
   const [passwordErrorText, setPasswordErrorText] = useState("");
 
-  // Add state for confirmPassword and confirmPasswordErrorText
-  const [confirmPassword, setConfrimPassword] = useState("");
-  const [confrimPasswordErrorText, setConfrimPasswordErrorText] = useState("");
-
   // Extra - add state for email and emailErrorText
   const [email, setEmail] = useState("");
   const [emailErrorText, setEmailErrorText] = useState("");
@@ -22,22 +18,13 @@ export default function RegistrationForm() {
   // Add state for isFormValid
   const [isFormValid, setIsFormValid] = useState(false);
 
-  // Add state to set formData
-  const [formData, setFormData] = useState(null); // For storing and displaying results
-
   // Add function to validateForm
   const validateForm = () => {
     const userNameValidatity = userName.length >= 3;
     const passwordValidity = password.length >= 8;
-    const confrimPasswordValidity = password === confirmPassword;
     const emailValidatity = email.length === 0 || /\S+@\S+\.\S+/.test(email);
 
-    setIsFormValid(
-      userNameValidatity &&
-        passwordValidity &&
-        confrimPasswordValidity &&
-        emailValidatity
-    );
+    setIsFormValid(userNameValidatity && passwordValidity && emailValidatity);
   };
 
   // Add function to validate username
@@ -68,20 +55,6 @@ export default function RegistrationForm() {
     validateForm();
   };
 
-  // Add function to validate confirm password
-  const validateConfrimPassword = (value) => {
-    if (value.length === 0) {
-      setConfrimPasswordErrorText("Comfrim Pasword is required");
-    } else if (value.length > 0 && value.length < 8) {
-      setConfrimPasswordErrorText(
-        "Confirm Password: Must match the password field."
-      );
-    } else {
-      setConfrimPasswordErrorText("");
-    }
-    validateForm();
-  };
-
   // Extra add function to validate email
   const validateEmail = (value) => {
     if (value.length > 0 && !/\S+@\S+\.\S+/.test(value)) {
@@ -106,13 +79,6 @@ export default function RegistrationForm() {
     validatePassword(value);
   };
 
-  // Add function to handle confirm password change
-  const handleConfrimPasswordChange = (e) => {
-    const value = e.target.value;
-    setConfrimPassword(value);
-    validateConfrimPassword(value);
-  };
-
   // Extra - Add function to handle email value change
   const handleEmail = (e) => {
     const value = e.target.value;
@@ -121,13 +87,27 @@ export default function RegistrationForm() {
   };
 
   // Create a handleSubmitFunction
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormData({
-      username: userName,
-      email: email || "N/A",
-    });
-    validateForm();
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      validateForm();
+      const response = await fetch("http://localhost:4000/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+          email: "ethan456@goldenbakery.ca",
+          password: "password12345",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(response);
+      const responsedata = await response.json();
+      console.log(responsedata);
+    } catch (error) {
+      console.error("Erorr fethcing data");
+    }
   };
 
   return (
@@ -136,7 +116,7 @@ export default function RegistrationForm() {
         {/* Form Section */}
         <div className="bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full">
           <h1 className="text-2xl font-bold text-blue-500 mb-6 text-center">
-            Registration Form
+            Log In
           </h1>
           <form onSubmit={handleSubmit} className="space-y-4">
             <div>
@@ -163,57 +143,6 @@ export default function RegistrationForm() {
             </div>
 
             <div>
-              <label htmlFor="password" className="block font-semibold mb-2">
-                Password:
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                className={`w-full p-2 bg-gray-900 text-white border rounded focus:outline-none focus:ring focus:ring-blue-500 
-                  ${
-                    passwordErrorText
-                      ? "border-red-500"
-                      : password && !passwordErrorText
-                      ? "border-green-500"
-                      : "border-gray-700"
-                  }`}
-                onChange={handlePasswordChange}
-              />
-              {passwordErrorText && (
-                <p className="text-red-500 text-sm mt-2">{passwordErrorText}</p>
-              )}
-            </div>
-
-            <div>
-              <label
-                htmlFor="confirmPassword"
-                className="block font-semibold mb-2"
-              >
-                Confirm Password:
-              </label>
-              <input
-                type="password"
-                id="confirmPassword"
-                value={confirmPassword}
-                className={`w-full p-2 bg-gray-900 text-white border rounded focus:outline-none focus:ring focus:ring-blue-500 
-                  ${
-                    confrimPasswordErrorText
-                      ? "border-red-500"
-                      : confirmPassword && !confrimPasswordErrorText
-                      ? "border-green-500"
-                      : "border-gray-700"
-                  }`}
-                onChange={handleConfrimPasswordChange}
-              />
-              {confrimPasswordErrorText && (
-                <p className="text-red-500 text-sm mt-2">
-                  {confrimPasswordErrorText}
-                </p>
-              )}
-            </div>
-
-            <div>
               <label htmlFor="email" className="block font-semibold mb-2">
                 Email (Optional):
               </label>
@@ -236,6 +165,29 @@ export default function RegistrationForm() {
               )}
             </div>
 
+            <div>
+              <label htmlFor="password" className="block font-semibold mb-2">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                className={`w-full p-2 bg-gray-900 text-white border rounded focus:outline-none focus:ring focus:ring-blue-500 
+                  ${
+                    passwordErrorText
+                      ? "border-red-500"
+                      : password && !passwordErrorText
+                      ? "border-green-500"
+                      : "border-gray-700"
+                  }`}
+                onChange={handlePasswordChange}
+              />
+              {passwordErrorText && (
+                <p className="text-red-500 text-sm mt-2">{passwordErrorText}</p>
+              )}
+            </div>
+
             <button
               type="submit"
               className={`w-full py-2 rounded ${
@@ -245,7 +197,7 @@ export default function RegistrationForm() {
               }`}
               disabled={!isFormValid}
             >
-              Register
+              Log In
             </button>
           </form>
         </div>
