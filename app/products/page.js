6899d530fddf9aef76b89053c49b613d2ec131da
Moksham <mokshamsharma09@gmail.com>
2024-12-01 22:47:20 -0700

    tweaks

--- app/products/page.js
+++ app/products/page.js
@@ -11,12 +11,12 @@ export default function Card() {
   return (
     <div>
       {/* Header Section */}
-      <h1 className="mb-6 text-5xl font-bold text-center">
+      <h1 className="mt-6 mb-6 font-bold text-center lg:text-5xl md:text-3xl">
         Delicious Bakery Products
       </h1>
 
       {/* Description */}
-      <div className="px-96">
+      <div className="px-40 lg:px-96">
         <p className="mb-6 font-semibold text-center text-md">
           Welcome to Our Bakery! Discover the joy of freshly baked bread,
           pastries, and buns, made with love and the finest ingredients. From
