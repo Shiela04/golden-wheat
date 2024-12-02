c1da7b112afe820815db21b5b1a55efafbe3eaae
Moksham <mokshamsharma09@gmail.com>
2024-12-01 22:39:08 -0700

    tweaks in the footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#d2b49c] text-black">
       <div className="grid grid-cols-1 gap-8 px-4 py-10 mx-auto max-w-7xl md:grid-cols-2 lg:grid-cols-4">
-        <div>
+        <div className="flex flex-col items-center">
           <h2 className="mb-4 text-lg font-bold">Connect With Us</h2>
           <ul className="space-y-4">
             <li className="flex items-center space-x-2">
@@ -12,37 +12,25 @@ export default function Footer() {
                 {" "}
                 <i className="text-xl text-black transition-colors fab fa-x-twitter hover:text-gray-200"></i>
               </a>
-              {/* <a href="#" className="hover:underline">
-                Twitter
-              </a> */}
             </li>
             <li className="flex items-center space-x-2">
               <a href="https://facebook.com/">
                 <i className="text-xl text-blue-600 transition-colors fab fa-facebook hover:text-blue-200"></i>
               </a>
-              {/* <a href="#" className="hover:underline">
-                Facebook
-              </a> */}
             </li>
             <li className="flex items-center space-x-2">
               <a href="https://instagram.com/">
                 <i className="text-xl text-pink-600 transition-colors fab fa-instagram hover:text-pink-200"></i>
               </a>
-              {/* <a href="#" className="hover:underline">
-                Instagram
-              </a> */}
             </li>
             <li className="flex items-center space-x-2">
               <a href="https://youtube.com/">
                 <i className="text-xl text-red-600 transition-colors fab fa-youtube hover:text-red-200"></i>
               </a>
-              {/* <a href="#" className="hover:underline">
-                YouTube
-              </a> */}
             </li>
           </ul>
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <h2 className="mb-4 text-lg font-bold">Quick Links</h2>
           <ul className="space-y-4">
             <li>
